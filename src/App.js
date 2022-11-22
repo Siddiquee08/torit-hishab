e92a7b366a203884b02f59fde0a5f39e4cb1e0da
Siddiquee08 <siddiquee09@gmail.com>
2022-11-22 15:57:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     <h1 className="text-3xl text-blue-400 mt-6 font-bold text-center">
+      তড়িৎ হিসাব
+    </h1>
+    <Input></Input>
+   </>
   );
 }
 
