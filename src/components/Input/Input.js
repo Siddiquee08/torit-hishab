164a4cd6959c2bed309d55d4026e963ecfa0ea00
Siddiquee08 <siddiquee09@gmail.com>
2022-11-23 13:04:03 +0600

    calculation

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Input.css";
 
 const Input = () => {
+  const [bill, setBill] = useState(0)
+  const submitForm = (event) => {
+    event.preventDefault()
+   const form = event.target
+   const watt = parseFloat(form.watt.value)
+   const hour = parseFloat(form.hour.value)
+   const unit = (watt * hour * 0.8) / 1000
+   const money = (unit * 24 * 0.7 * 10.3).toFixed(2)
+   setBill(money)
+   form.reset()
+  }
   return (
     <>
-      <form>
+      <form onSubmit={submitForm}>
         <div className="form">
           <div className="hero mt-3">
             <div>
@@ -13,13 +24,14 @@ const Input = () => {
                   <div className="form-control">
                     <label className="label">
                       <span className="label-text">
-                        ব্যবহৃত ইউনিট/ইলেক্ট্রিক যন্ত্রটির ওয়াট রেটিং
+                        ব্যবহৃত ইলেক্ট্রিক যন্ত্রটির ওয়াট রেটিং
                       </span>
                     </label>
                     <input
                       type="text"
                       placeholder="ওয়াট"
-                      className="input input-bordered"
+                      name="watt"
+                      className="input input-bordered text-center"
                     />
                   </div>
                   <div className="form-control">
@@ -29,13 +41,18 @@ const Input = () => {
                     <input
                       type="text"
                       placeholder="ঘন্টা"
-                      className="input input-bordered"
+                      name="hour"
+                      className="input input-bordered text-center"
                     />
                   </div>
+                  <div className="form-control">
+                    <label className="label">
+                      <span className="label-text">আপনার সম্ভাব্য মাসিক বিল হলো, টাকাঃ</span>
+                    </label>
+                    <input type="text" value={bill} className="input input-bordered text-center" />
+                  </div>
                   <div className="form-control mt-6">
-                    <button className="btn btn-primary bg-blue-400 border-none text-end">
-                      হিসাব
-                    </button>
+                  <input type="submit" value="হিসাব" className="btn btn-primary bg-blue-400 border-none text-end" />
                   </div>
                 </div>
               </div>
